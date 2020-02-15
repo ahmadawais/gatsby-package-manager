9d4fad4aaa650975c71a2216fad5373ffc3a9a5c
Ahmad Awais <me@AhmadAwais.com>
2020-02-16 03:00:01 +0500

    📦 NEW: Prompts

--- index.js
+++ index.js
@@ -7,6 +7,25 @@ process.on("unhandledRejection", err => {
 	handleError(`UNHANDLED`, err);
 });
 
+	const promptClone = new Toggle({
+		name: `root`,
+		message: `Are you running this in the root directory of a Gatsby project?`
+	});
+	const [errRoot, root] = await to(promptClone.run());
+	handleError(`ROOT`, errRoot);
+	await shouldCancel(root);
+
+	// Slug.
+	const promptName = {
+		type: `input`,
+		name: `name`,
+		initial: `gatsby-plugin-xyz`,
+		message: `Gatsby package you'd like to install?`
+	};
+	const [errName, name] = await to(prompt(promptName));
+	handleError(`NAME`, errName);
+	await shouldCancel(name);
+	const pkgSlug = name.name;
 
 /**
  * Gatsby Package Manager.
