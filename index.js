6487355a0a6fd0f5984a9908f1b44ae1efa2d59b
Ahmad Awais <me@AhmadAwais.com>
2020-02-16 02:59:44 +0500

    📦 NEW: Unhandled

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-/* GATSBY PACKAGE MANAGER */
+#!/usr/bin/env node
+
+// Makes the script crash on unhandled rejections instead of silently
+// ignoring them. In the future, promise rejections that are not handled will
+// terminate the Node.js process with a non-zero exit code.
+process.on("unhandledRejection", err => {
+	handleError(`UNHANDLED`, err);
+});
+
 
 /**
  * Gatsby Package Manager.
